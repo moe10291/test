0086f98b6bd3bea9db4e9b704a3ad39d076546fc
moe10291 <mohammad57zero@gmail.com>
2020-11-06 21:33:15 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
+            <h1>Does it work the way it is supposed to ?</h1>
+            <h2>This should also work</h2>
           </p>
           <a
             className="App-link"
